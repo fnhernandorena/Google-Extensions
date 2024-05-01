e7a60ebdd5d0282a96fe1ba9fcc6f6d8c1fb9d73
Francisco Nicolas Hernandorena <fnhernandorena@gmail.com>
2024-05-01 14:37:28 -0300

    clean

--- Image-gallery/popup.js
+++ Image-gallery/popup.js
@@ -1,18 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {action: "getImages"}, function(response) {
-        const images = response.images;
-        const container = document.getElementById('imageContainer');
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {action: "getImages"}, function(response) {
+      const images = response ? response.images : [];
+      const container = document.getElementById('imageContainer');
+
+      if (images.length === 0) {
+        const noImagesMessage = document.createElement('p');
+        noImagesMessage.textContent = "No images found.";
+        container.appendChild(noImagesMessage);
+      } else {
         images.forEach(function(imageUrl) {
           const link = document.createElement('a');
           link.href = imageUrl;
-          link.target = "_blank"; 
+          link.target = "_blank"; // Abre la imagen en una nueva pestaña
           const img = document.createElement('img');
           img.src = imageUrl;
           link.appendChild(img);
           container.appendChild(link);
         });
-      });
+      }
     });
   });
-  
+});
